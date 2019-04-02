9e708d2779da5368af44c5dcf76a17ec4ac13f3a
Adrian Rodriguez <arodriguez@cs.stanford.edu>
2019-04-02 21:32:20 +0700

    Update sample_solution.js

--- sample_solution.js
+++ sample_solution.js
@@ -3,91 +3,68 @@
  * Implement a solver function. Should be called by another file.
  */
 
-// solver function
-// take a cube object as an input and manipulate it
-function solve(cube) {
-
-// white cross
-/*
-// for each side color
-cube.middleColors.forEach((color) => {
-  // focus on Up Right edge
-  // TODO: shorten function names?
-  //var focusColor = cube.getCorrectPieceColor('UR')
-  var focucColor = 'W'+color
-  var currentPosition = cube.findPiece(focusColor)
-  cube.moveToUpRightFrom(currentPosition)
-  if (cube.edgeIsReversed('UR')) {
-    cube.move("R' U F' U'")
-  }
-  
-  cube.move('y')
-})
-*/
-
 cube.setStart()
 
-var piece
+let solveWhiteCross = () => {
+  let wbEdge = cube.findPiece("WB")
+  cube.moveToUpFront(wbEdge)
+  if (cube.edgeIsReversed(wbEdge)) {
+    cube.rotateCCW()
+    cube.move("R' U F' U'")
+    cube.rotateCW()
+  }
+  
+  cube.rotateCW()
 
-piece = cube.findPiece("WB")
-cube.moveToUpFront(piece)
-if (cube.edgeIsReversed(piece)) {
-  cube.rotateCCW()
-  cube.move("R' U F' U'")
+  let woEdge = cube.findPiece("WO")
+  cube.moveToUpFront(woEdge)
+  if (cube.edgeIsReversed(woEdge)) {
+    cube.rotateCCW()
+    cube.move("R' U F' U'")
+    cube.rotateCW()
+  }
+
+  cube.rotateCW()
+
+  let wgEdge = cube.findPiece("WG")
+  cube.moveToUpFront(wgEdge)
+  if (cube.edgeIsReversed(wgEdge)) {
+    cube.rotateCCW()
+    cube.move("R' U F' U'")
+    cube.rotateCW()
+  }
+
+  cube.rotateCW()
+
+  let wrEdge = cube.findPiece("WR")
+  cube.moveToUpFront(wrEdge)
+  if (cube.edgeIsReversed(wrEdge)) {
+    cube.rotateCCW()
+    cube.move("R' U F' U'")
+    cube.rotateCW()
+  }
+
   cube.rotateCW()
 }
 
-cube.rotateCW()
+  // done white cross
 
-piece = cube.findPiece("WR")
-cube.moveToUpFront(piece)
-if (cube.edgeIsReversed(piece)) {
-  cube.rotateCCW()
-  cube.move("R' U F' U'")
-  cube.rotateCW()
-}
+  // start white corners
 
-cube.rotateCW()
+  // piece = cube.findPiece('WRB')
+  // if (piece.includes('U')) { // make wrapper function
+  //   cube.moveFromUpToDown(piece)
+  // }
+  // while (cube.findPiece('WRB') != 'DFR') { // make wrapper function?
+  //   cube.move('D')
+  // }
+  // while (cube.cornerUpFrontRightNotCorrect('WRB')) {
+  //   cube.move("R' D' R D")
+  // }
 
-piece = cube.findPiece("WG")
-cube.moveToUpFront(piece)
-if (cube.edgeIsReversed(piece)) {
-  cube.rotateCCW()
-  cube.move("R' U F' U'")
-  cube.rotateCW()
-}
+  // cube.rotateCW()
 
-cube.rotateCW()
-
-piece = cube.findPiece("WO")
-cube.moveToUpFront(piece)
-if (cube.edgeIsReversed(piece)) {
-  cube.rotateCCW()
-  cube.move("R' U F' U'")
-  cube.rotateCW()
-}
-
-cube.rotateCW()
-
-// done white cross
-
-// start white corners
-
-piece = cube.findPiece('WRB')
-if (piece.includes('U')) { // make wrapper function
-  cube.moveFromUpToDown(piece)
-}
-while (cube.findPiece('WRB') != 'DFR') { // make wrapper function?
-  cube.move('D')
-}
-while (cube.cornerUpFrontRightNotCorrect('WRB')) {
-  cube.move("R' D' R D")
-}
-
-cube.rotateCW()
-
-// done white corners
+  // done white corners
 
 }
 
-module.exports = solve
