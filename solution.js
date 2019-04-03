e8998ade776b55f6c79d7c78ca0ba9e2925f7f16
Oras Phongpanangam <panangam@gmail.com>
2019-04-03 01:09:38 -0700

    add solution up to white corners

--- solution.js
+++ solution.js
@@ -6,6 +6,8 @@ cube.setStart()
 
 var piece
 
+// start white cross
+
 piece = cube.findPiece("WB")
 cube.moveToUpFront(piece)
 if (cube.edgeIsReversed("UF")) {
@@ -47,6 +49,62 @@ if (cube.edgeIsReversed("UF")) {
 cube.rotateCW()
 
 // done white cross
+
+// start white corners
+
+piece = cube.findPiece('WRB')
+if (piece.includes('U')) { // make wrapper function
+  cube.moveFromUpToDown(piece)
+}
+while (cube.findPiece('WRB') != 'DFR') { // make wrapper function?
+  cube.move('D')
+}
+while (cube.cornerUpFrontRightNotCorrect('WRB')) {
+  cube.move("R' D' R D")
+}
+
+cube.rotateCW()
+
+piece = cube.findPiece('WGR')
+if (piece.includes('U')) { // make wrapper function
+  cube.moveFromUpToDown(piece)
+}
+while (cube.findPiece('WGR') != 'DFR') { // make wrapper function?
+  cube.move('D')
+}
+while (cube.cornerUpFrontRightNotCorrect('WGR')) {
+  cube.move("R' D' R D")
+}
+
+cube.rotateCW()
+
+piece = cube.findPiece('WOG')
+if (piece.includes('U')) { // make wrapper function
+  cube.moveFromUpToDown(piece)
+}
+while (cube.findPiece('WOG') != 'DFR') { // make wrapper function?
+  cube.move('D')
+}
+while (cube.cornerUpFrontRightNotCorrect('WOG')) {
+  cube.move("R' D' R D")
+}
+
+cube.rotateCW()
+
+piece = cube.findPiece('WBO')
+if (piece.includes('U')) { // make wrapper function
+  cube.moveFromUpToDown(piece)
+}
+while (cube.findPiece('WBO') != 'DFR') { // make wrapper function?
+  cube.move('D')
+}
+while (cube.cornerUpFrontRightNotCorrect('WBO')) {
+  cube.move("R' D' R D")
+}
+
+cube.rotateCW()
+
+// done white corners
 
 }
 
