4991dd7b9c9c5e543a5e8056d592e6896ad2ad12
Oras Phongpanangam <panangam@gmail.com>
2019-04-03 00:26:47 -0700

    add solution skeleton

---
+++ solution.js
@@ -0,0 +1,10 @@
+// function to solve a cube
+// take a Rubiks object as an input and manipulate it directly
+function solve(cube) {
+
+
+
+}
+
+// export solve function out to be integrated elsewhere
+module.exports = solve
