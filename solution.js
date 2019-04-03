e1507e84a942bc8debf8227a9ae54f2a5c850fe8
Oras Phongpanangam <panangam@gmail.com>
2019-04-03 00:48:03 -0700

    add solution up to white cross

--- solution.js
+++ solution.js
@@ -2,7 +2,51 @@
 // take a Rubiks object as an input and manipulate it directly
 function solve(cube) {
 
+cube.setStart()
 
+var piece
+
+piece = cube.findPiece("WB")
+cube.moveToUpFront(piece)
+if (cube.edgeIsReversed("UF")) {
+  cube.rotateCCW()
+  cube.move("R' U F' U'")
+  cube.rotateCW()
+}
+
+cube.rotateCW()
+
+piece = cube.findPiece("WR")
+cube.moveToUpFront(piece)
+if (cube.edgeIsReversed("UF")) {
+  cube.rotateCCW()
+  cube.move("R' U F' U'")
+  cube.rotateCW()
+}
+
+cube.rotateCW()
+
+piece = cube.findPiece("WG")
+cube.moveToUpFront(piece)
+if (cube.edgeIsReversed("UF")) {
+  cube.rotateCCW()
+  cube.move("R' U F' U'")
+  cube.rotateCW()
+}
+
+cube.rotateCW()
+
+piece = cube.findPiece("WO")
+cube.moveToUpFront(piece)
+if (cube.edgeIsReversed("UF")) {
+  cube.rotateCCW()
+  cube.move("R' U F' U'")
+  cube.rotateCW()
+}
+
+cube.rotateCW()
+
+// done white cross
 
 }
 
