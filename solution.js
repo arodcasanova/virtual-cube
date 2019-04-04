444e4950126fdd254b862f3c1fc9318f6dfd6534
Adrian Rodriguez <arodriguez@cs.stanford.edu>
2019-04-04 14:31:00 +0700

    Add updated solution for students

--- solution.js
+++ solution.js
@@ -2,45 +2,4 @@ let rubiks = require("./src/rubiks.js")
 let cube = new rubiks()
 
 cube.randomize()
-cube.setStart()
-
-let piece = cube.findPiece("WB")
-cube.moveToUpFront(piece)
-if (cube.edgeIsReversed("UF")) {
-  cube.rotateCCW()
-  cube.move("R' U F' U'")
-  cube.rotateCW()
-}
-
-cube.rotateCW()
-
-piece = cube.findPiece("WR")
-cube.moveToUpFront(piece)
-if (cube.edgeIsReversed("UF")) {
-  cube.rotateCCW()
-  cube.move("R' U F' U'")
-  cube.rotateCW()
-}
-
-cube.rotateCW()
-
-piece = cube.findPiece("WG")
-cube.moveToUpFront(piece)
-if (cube.edgeIsReversed("UF")) {
-  cube.rotateCCW()
-  cube.move("R' U F' U'")
-  cube.rotateCW()
-}
-
-cube.rotateCW()
-
-piece = cube.findPiece("WO")
-cube.moveToUpFront(piece)
-if (cube.edgeIsReversed("UF")) {
-  cube.rotateCCW()
-  cube.move("R' U F' U'")
-  cube.rotateCW()
-}
-
-cube.rotateCW()
 cube.check()
