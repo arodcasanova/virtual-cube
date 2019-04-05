607f5ca1a68f68859ba121467d2e6a546605caf8
Adrian Rodriguez <arodriguez@cs.stanford.edu>
2019-04-05 11:15:35 +0700

    Clean solution.js

--- solution.js
+++ solution.js
@@ -4,15 +4,6 @@ let cube = new rubiks()
 cube.randomize()
 cube.setStart()
 
-cube.whiteEdgesNames().forEach(edgeName => {
-  edge = cube.findPiece(edgeName)
-  cube.moveToUpFront(edge)
-  if (cube.edgeIsReversed("UF")) {
-      cube.rotateCCW()
-      cube.move("R' U F' U'")
-      cube.rotateCW()
-  }
-  cube.rotateCW()
-})
+
 
 cube.check()
